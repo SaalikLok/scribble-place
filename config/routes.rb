2700e2f54094cef7c948db60edded03eab183c71
Saalik Lokhandwala <saalikl111@gmail.com>
2023-09-07 13:36:43 -0500

    User-set timezones (#2)
    * add: timezone to user and scribble UI
    
    * readme and landing page changes
    
    * basic styling for registration screens
    
    * update account time zone

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "homes#index"
   get "/home", to: "homes#index"
   
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
   
   resources :scribbles, except: :show
 end
