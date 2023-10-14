c6013a8041ddd40ab170eaf3a4d2f88f1d06a309
Saalik Lokhandwala <saalikl111@gmail.com>
2023-10-14 09:02:19 +0300

    add spec: scribble model

--- spec/models/scribble_spec.rb
+++ spec/models/scribble_spec.rb
@@ -1,5 +1,12 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Scribble, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(build(:scribble)).to be_valid
+  end
+
+  it "must have some content" do
+    scribble = build(:scribble, content: nil)
+    expect(scribble).to_not be_valid
+  end
 end
