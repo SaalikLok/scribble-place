c73b8b05308e7d439613e4645cabc83ff2fbf4a6
Saalik Lokhandwala <saalikl111@gmail.com>
2023-10-20 01:53:50 +0300

    Loading paginated Scribbles (#4)
    * configure pagy and faker
    
    * "load more" button, endless pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+require "faker"
+
+User.delete_all
+Scribble.delete_all
 
 user = User.create(email: "superscribbler@lokbros.com", password: "password")
-Scribble.create(content: "This is my scribble", user_id: user.id)
+
+50.times do
+  Scribble.create(content: Faker::Quote.yoda, user_id: user.id)
+end
