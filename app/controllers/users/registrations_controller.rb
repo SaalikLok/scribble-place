8263814ab0ecf066e02761d7020dd8f6f6c51356
Saalik Lokhandwala <saalikl111@gmail.com>
2024-05-04 14:39:18 +0800

    disable new user registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,16 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def new
+    flash[:alert] = "New registrations are not allowed."
+    redirect_to root_path
+  end
+
+  def create
+    flash[:alert] = "New registrations are not allowed."
+    redirect_to root_path
+  end
+
   protected
 
   def configure_permitted_parameters
